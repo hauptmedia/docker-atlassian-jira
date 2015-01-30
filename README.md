037862bbd1b9cb36cb0629acc60e3be776298b82
root <root@server4.spielmeister.com>
2015-01-30 14:37:20 +0100

    removed JIRA_ prefix from env variables

--- README.md
+++ README.md
@@ -25,11 +25,11 @@ hauptmedia/atlassian-jira-data
 
 # jira
 docker run \
--e JIRA_CONNECTOR_PROXYNAME=jira.$DOMAIN \
--e JIRA_CONNECTOR_PROXYPORT=80 \
--e JIRA_CONNECTOR_SECURE=false \
--e JIRA_CONNECTOR_SCHEME=http \
--e JIRA_CONTEXT_PATH=/ \
+-e CONNECTOR_PROXYNAME=jira.$DOMAIN \
+-e CONNECTOR_PROXYPORT=80 \
+-e CONNECTOR_SECURE=false \
+-e CONNECTOR_SCHEME=http \
+-e CONTEXT_PATH=/ \
 --name ${NAME_PREFIX}atlassian-jira \
 --volumes-from ${NAME_PREFIX}atlassian-jira-data \
 --link ${NAME_PREFIX}atlassian-mysql:mysql \
@@ -58,7 +58,7 @@ docker run --link atlassian-mysql:mysql -d hauptmedia/atlassian-jira
 
 ### Reverse proxy setup
 
-Jira needs a special setup when it's run behind a reverse proxy. These settings are exposed via the environment variables *JIRA_CONNECTOR_PROXYNAME*, *JIRA_CONNECTOR_PROXYPORT*, *JIRA_CONNECTOR_SECURE*, *JIRA_CONNECTOR_SCHEME* and *JIRA_CONTEXT_PATH*.
+Jira needs a special setup when it's run behind a reverse proxy. These settings are exposed via the environment variables *CONNECTOR_PROXYNAME*, *CONNECTOR_PROXYPORT*, *CONNECTOR_SECURE*, *CONNECTOR_SCHEME* and *CONTEXT_PATH*.
 
 See http & https examples below.
 
@@ -70,22 +70,22 @@ Note: if you link the **hauptmedia/atlassian-reverseproxy** container with this 
 
 ```bash
 docker run -d hauptmedia/atlassian-jira \
--e JIRA_CONNECTOR_PROXYNAME=jira.example.com \
--e JIRA_CONNECTOR_PROXYPORT=80 \
--e JIRA_CONNECTOR_SECURE=false \
--e JIRA_CONNECTOR_SCHEME=http \
--e JIRA_CONTEXT_PATH=/
+-e CONNECTOR_PROXYNAME=jira.example.com \
+-e CONNECTOR_PROXYPORT=80 \
+-e CONNECTOR_SECURE=false \
+-e CONNECTOR_SCHEME=http \
+-e CONTEXT_PATH=/
 ```
 
 #### https
 
 ```bash
 docker run -d hauptmedia/atlassian-jira \
--e JIRA_CONNECTOR_PROXYNAME=jira.example.com \
--e JIRA_CONNECTOR_PROXYPORT=443 \
--e JIRA_CONNECTOR_SECURE=true \
--e JIRA_CONNECTOR_SCHEME=https \
--e JIRA_CONTEXT_PATH=/
+-e CONNECTOR_PROXYNAME=jira.example.com \
+-e CONNECTOR_PROXYPORT=443 \
+-e CONNECTOR_SECURE=true \
+-e CONNECTOR_SCHEME=https \
+-e CONTEXT_PATH=/
 ```
 
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -45,29 +45,29 @@ if [ -n "$MYSQL_NAME" ]; then
 EOF
 fi
 
-if [ -n "$JIRA_CONNECTOR_PROXYNAME" ]; then
+if [ -n "$CONNECTOR_PROXYNAME" ]; then
 	xmlstarlet ed --inplace --delete "/Server/Service/Connector/@proxyName" $JIRA_INSTALL_DIR/conf/server.xml
-	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n proxyName -v $JIRA_CONNECTOR_PROXYNAME $JIRA_INSTALL_DIR/conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n proxyName -v $CONNECTOR_PROXYNAME $JIRA_INSTALL_DIR/conf/server.xml
 fi
 
-if [ -n "$JIRA_CONNECTOR_PROXYPORT" ]; then
+if [ -n "$CONNECTOR_PROXYPORT" ]; then
 	xmlstarlet ed --inplace --delete "/Server/Service/Connector/@proxyPort" $JIRA_INSTALL_DIR/conf/server.xml
-	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n proxyPort -v $JIRA_CONNECTOR_PROXYPORT $JIRA_INSTALL_DIR/conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n proxyPort -v $CONNECTOR_PROXYPORT $JIRA_INSTALL_DIR/conf/server.xml
 fi
 
-if [ -n "$JIRA_CONNECTOR_SECURE" ]; then
+if [ -n "$CONNECTOR_SECURE" ]; then
 	xmlstarlet ed --inplace --delete "/Server/Service/Connector/@secure" $JIRA_INSTALL_DIR/conf/server.xml
-	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n secure -v $JIRA_CONNECTOR_SECURE $JIRA_INSTALL_DIR/conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n secure -v $CONNECTOR_SECURE $JIRA_INSTALL_DIR/conf/server.xml
 fi
 
-if [ -n "$JIRA_CONNECTOR_SCHEME" ]; then
+if [ -n "$CONNECTOR_SCHEME" ]; then
 	xmlstarlet ed --inplace --delete "/Server/Service/Connector/@scheme" $JIRA_INSTALL_DIR/conf/server.xml
-	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n scheme -v $JIRA_CONNECTOR_SCHEME $JIRA_INSTALL_DIR/conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Connector" --type attr -n scheme -v $CONNECTOR_SCHEME $JIRA_INSTALL_DIR/conf/server.xml
 fi
 
-if [ -n "$JIRA_CONTEXT_PATH" ]; then
+if [ -n "$CONTEXT_PATH" ]; then
 	xmlstarlet ed --inplace --delete "/Server/Service/Engine/Host/Context/@path" $JIRA_INSTALL_DIR/conf/server.xml
-	xmlstarlet ed --inplace --insert "/Server/Service/Engine/Host/Context" --type attr -n path -v $JIRA_CONTEXT_PATH $JIRA_INSTALL_DIR/conf/server.xml
+	xmlstarlet ed --inplace --insert "/Server/Service/Engine/Host/Context" --type attr -n path -v $CONTEXT_PATH $JIRA_INSTALL_DIR/conf/server.xml
 fi
 	
 exec "$@"
